7b838384527d4489dd5071c4b64f2e4f7fc1b626
PakSemen <117230189+PakSemen@users.noreply.github.com>
2023-07-06 21:16:22 +0300

    Update UserDao.java

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/UserDao.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/UserDao.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.javafilmorate.storage;
 import ru.yandex.practicum.javafilmorate.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserDao {
 
